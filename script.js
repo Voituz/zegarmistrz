81f75abb476f88a2bba2079bbf399a55d08ddf02
Wojciech Bylina <w.bylina93@gmail.com>
2020-11-21 16:19:53 +0100

    viewport fixes

--- script.js
+++ script.js
@@ -140,8 +140,8 @@ document.querySelector('#form-submit').addEventListener('click', function(event)
 })
 
 // viewport stabilizing
-const initialHeight = '100vh';
+// const initialHeight = '100';
 
- document.documentElement.style.setProperty('overflow', 'auto')
- const metaViewport = document.querySelector('meta[name=viewport]')
- metaViewport.setAttribute('content', 'height=' + initialHeight + 'px, width=device-width, initial-scale=1.0');
+//  document.documentElement.style.setProperty('overflow', 'auto')
+//  const metaViewport = document.querySelector('meta[name=viewport]')
+//  metaViewport.setAttribute('content', 'height=' + initialHeight + 'px, width=device-width, initial-scale=1.0');
